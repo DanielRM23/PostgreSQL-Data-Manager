644f5b490f03bf8cf52915ce38106792f43befcb
DanielRM23 <DanielRM@ciencias.unam.mx>
2024-05-06 00:43:54 -0600

    Parte 3 del proyecto. Se añade la función de leer información y se agregan comentarios DocString.

--- DjangoFBD/tasks/urls.py
+++ DjangoFBD/tasks/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path
-from .views import list_tasks, createHotel, deleteHotel, editHotelRender, editHotel
+from .views import (list_tasks, createHotel, deleteHotel,
+                    editHotelRender, editHotel, readHotelRender, readHotel)
 
+# Lista de rutas para la aplicación de gestión de hoteles
 urlpatterns = [
-    # Página principal
-    path('', list_tasks),
-    # Para crear un hotel
+    # Muestra la página principal con todos los hoteles
+    path('', list_tasks, name="list_tasks"),
+
+    # Ruta para crear un nuevo hotel mediante una solicitud POST
     path('new/', createHotel, name="createHotel"),
-    # Para eliminar un hotel
+
+    # Ruta para eliminar un hotel existente por su ID
     path('delete_hotel/<int:hotel_id>/', deleteHotel, name='deleteHotel'),
-    # Render para editar un hotel
+
+    # Ruta para mostrar el formulario de edición para un hotel específico
     path('edit_hotel_Render/<int:hotel_id>/', editHotelRender, name="editHotelRender"),
-    # Para editar un hotel
+
+    # Ruta para procesar la actualización de un hotel por su ID mediante POST
     path('edit_hotel/<int:hotel_id>/', editHotel, name="editHotel"),
 
+    # Ruta para mostrar los detalles de un hotel específico
+    path('read_hotel_Render/<int:hotel_id>/', readHotelRender, name="readHotelRender"),
+
+    # Ruta para editar los detalles de un hotel específico (similar a `editHotel`, pero
+    # podría ser usada para ver o editar detalles dependiendo del contexto)
+    path('read_hotel/<int:hotel_id>/', readHotel, name="readHotel"),
 ]
--- DjangoFBD/tasks/views.py
+++ DjangoFBD/tasks/views.py
@@ -2,19 +2,40 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseBadRequest
 from .models import Hotel
 
-# Create your views here.
-
 
 def list_tasks(request):
-    Hotels = Hotel.objects.all()
-    return render(request, "list_tasks.html", {'Hotels': Hotels})
+    '''
+    Muestra todos los hoteles disponibles.
+
+    Esta función obtiene todos los objetos 'Hotel' almacenados en la base de datos y
+    los proporciona al contexto para renderizar la plantilla 'list_tasks.html'.
+
+    :param request: Una instancia del objeto HttpRequest que representa la solicitud del cliente.
+    :return: Una respuesta HTTP con el contenido renderizado de 'list_tasks.html',
+             incluyendo todos los hoteles disponibles en el contexto.
+    '''
+    Hotels = Hotel.objects.all()  # Obtiene todos los objetos Hotel de la base de datos
+    return render(request, "list_tasks.html", {'Hotels': Hotels})  # Renderiza la vista con el contexto de los hoteles
 
 
-# función que crea un hotel
+
 def createHotel(request):
-    if request.method == 'POST':
-        # Todos los campos requeridos están presentes
-        required_fields = [
+    '''
+    Crea un nuevo hotel con datos proporcionados en una solicitud POST.
+
+    Esta función espera que la solicitud sea de tipo POST y contenga ciertos campos
+    requeridos para crear un nuevo objeto 'Hotel'. Verifica que todos los campos necesarios
+    estén presentes y, si alguno falta, devuelve una respuesta de error. Si todos los
+    campos están presentes, se crea un nuevo objeto 'Hotel' y se guarda en la base de datos.
+    Tras el guardado exitoso, se redirige a la página de inicio.
+
+    :param request: Una instancia de HttpRequest que representa la solicitud entrante desde el cliente.
+    :return: Una respuesta HTTP que puede ser:
+             - HTTP 302 para redirigir a la página de inicio tras guardar con éxito.
+             - HTTP 400 si falta algún campo requerido o la solicitud no es de tipo POST.
+    '''
+    if request.method == 'POST':  # Verifica si la solicitud es de tipo POST
+        required_fields = [  # Lista de campos requeridos para la creación del hotel
             "idHotel",
             "nombreEstablecimiento",
             "horaCheckIn",
@@ -28,69 +49,169 @@ def createHotel(request):
             "estado"
         ]
 
-        # Verificar que todos los campos requeridos están presentes
+        # Verifica que todos los campos requeridos están presentes en la solicitud POST
         for field in required_fields:
             if field not in request.POST:
-                return HttpResponseBadRequest(f"Falta el campo '{field}'.")
+                return HttpResponseBadRequest(f"Falta el campo '{field}'.")  # Devuelve un error si falta un campo
 
-        # Crear un nuevo objeto Hotel con los valores del POST
+        # Crea un nuevo objeto Hotel con los datos de la solicitud POST
         nuevo_hotel = Hotel(
-            idHotel=request.POST.get("idHotel"),
-            nombreEstablecimiento=request.POST.get("nombreEstablecimiento"),
-            horaCheckIn=request.POST.get("horaCheckIn"),
-            horaCheckOut=request.POST.get("horaCheckOut"),
-            petFriendly=request.POST.get("petFriendly") == 'on',  # Convertir a booleano
-            servicio=request.POST.get("servicio"),
-            numeroInterior=request.POST.get("numeroInterior"),  # Puede ser None
-            numeroExterior=int(request.POST.get("numeroExterior")),  # Convertir a entero
-            colonia=request.POST.get("colonia"),
-            calle=request.POST.get("calle"),
-            estado=request.POST.get("estado")
+            idHotel=request.POST.get("idHotel"),  # Obtiene el valor del campo 'idHotel' del POST
+            nombreEstablecimiento=request.POST.get("nombreEstablecimiento"),  # Obtiene el nombre del establecimiento
+            horaCheckIn=request.POST.get("horaCheckIn"),  # Hora de check-in
+            horaCheckOut=request.POST.get("horaCheckOut"),  # Hora de check-out
+            petFriendly=request.POST.get("petFriendly") == 'on',  # Convierte el valor a booleano
+            servicio=request.POST.get("servicio"),  # Obtiene el tipo de servicio
+            numeroInterior=request.POST.get("numeroInterior"),  # Puede ser nulo, número de interior
+            numeroExterior=int(request.POST.get("numeroExterior")),  # Convierte a entero, número de exterior
+            colonia=request.POST.get("colonia"),  # Colonia
+            calle=request.POST.get("calle"),  # Calle
+            estado=request.POST.get("estado")  # Estado
         )
 
-        # Guardar el objeto en la base de datos
+        # Guarda el nuevo objeto Hotel en la base de datos
         nuevo_hotel.save()
 
-        # Redirigir después de guardar con éxito
-        return redirect('/')
+        # Redirige a la página principal después de guardar con éxito
+        return redirect('/')  # Redirige a la página de inicio
 
     else:
-        # Manejo de solicitudes que no sean POST
-        return HttpResponseBadRequest("Solo se aceptan solicitudes POST.")
+        # Devuelve un error si la solicitud no es de tipo POST
+        return HttpResponseBadRequest("Solo se aceptan solicitudes POST.")  # Solo acepta solicitudes POST
+
 
 
 def deleteHotel(request, hotel_id):
-    hotel_deleted = Hotel.objects.get(idHotel=hotel_id)
-    hotel_deleted.delete()
-    return redirect('/')
+    '''
+    Elimina un hotel dado su identificador único.
+
+    Esta función elimina un objeto 'Hotel' de la base de datos utilizando su identificador único
+    proporcionado como argumento. Tras eliminar el hotel, redirige a la página de inicio.
+
+    :param request: Una instancia de HttpRequest que representa la solicitud entrante desde el cliente.
+    :param hotel_id: El identificador único del hotel que se desea eliminar.
+    :return: Una respuesta HTTP que redirige a la página de inicio después de eliminar el hotel.
+    '''
+    hotel_deleted = Hotel.objects.get(idHotel=hotel_id)  # Obtiene el hotel a eliminar por su id
+    hotel_deleted.delete()  # Elimina el objeto Hotel de la base de datos
+    return redirect('/')  # Redirige a la página de inicio después de eliminar
+
+
 
 def editHotelRender(request, hotel_id):
-    hotel_edited = Hotel.objects.get(idHotel=hotel_id)
-    return render(request, "edicionHotel.html", {"hotel_edited": hotel_edited})
+    '''
+    Renderiza el formulario para editar un hotel.
+
+    Esta función obtiene un objeto 'Hotel' de la base de datos utilizando su identificador único
+    y lo pasa al contexto para renderizar la plantilla 'edicionHotel.html', donde se mostrará el
+    formulario para editar los detalles del hotel.
+
+    :param request: Una instancia de HttpRequest que representa la solicitud entrante desde el cliente.
+    :param hotel_id: El identificador único del hotel que se desea editar.
+    :return: Una respuesta HTTP que renderiza la plantilla 'edicionHotel.html' con el hotel a editar
+             en el contexto.
+    '''
+    hotel_edited = Hotel.objects.get(idHotel=hotel_id)  # Obtiene el hotel a editar
+    return render(request, "edicionHotel.html",
+                  {"hotel_edited": hotel_edited})  # Renderiza la vista de edición con el hotel a editar
+
 
 
 def editHotel(request, hotel_id):
-    hotel = get_object_or_404(Hotel, idHotel=hotel_id)
+    '''
+    Actualiza la información de un hotel existente.
 
-    if request.method == 'POST':
-        # Actualizar el objeto existente
+    Esta función obtiene un objeto 'Hotel' por su identificador único. Si el hotel existe,
+    actualiza sus detalles con la información proporcionada en una solicitud POST.
+    Si la solicitud no es de tipo POST, se devuelve un error.
+
+    :param request: Una instancia de HttpRequest que representa la solicitud entrante desde el cliente.
+    :param hotel_id: El identificador único del hotel que se desea editar.
+    :return: Una respuesta HTTP que puede ser:
+             - Redirección a la página principal tras actualizar con éxito.
+             - HTTP 400 si la solicitud no es de tipo POST.
+    '''
+    hotel = get_object_or_404(Hotel, idHotel=hotel_id)  # Obtiene el objeto Hotel por su id, o retorna 404 si no existe
+
+    if request.method == 'POST':  # Verifica si la solicitud es de tipo POST
+        # Actualiza el objeto Hotel con nuevos datos obtenidos del POST
         hotel.nombreEstablecimiento = request.POST.get("nombreEstablecimiento")
         hotel.horaCheckIn = request.POST.get("horaCheckIn")
         hotel.horaCheckOut = request.POST.get("horaCheckOut")
-        hotel.petFriendly = request.POST.get("petFriendly") == 'on'
+        hotel.petFriendly = request.POST.get("petFriendly") == 'on'  # Convierte a booleano
         hotel.servicio = request.POST.get("servicio")
         hotel.numeroInterior = request.POST.get("numeroInterior")
-        hotel.numeroExterior = int(request.POST.get("numeroExterior"))
+        hotel.numeroExterior = int(request.POST.get("numeroExterior"))  # Convierte a entero
         hotel.colonia = request.POST.get("colonia")
         hotel.calle = request.POST.get("calle")
         hotel.estado = request.POST.get("estado")
 
-        # Guardar cambios
+        # Guarda los cambios en la base de datos
         hotel.save()
 
-        # Redirigir después de la edición
-        return redirect('/')
+        # Redirige a la página principal después de editar con éxito
+        return redirect('/')  # Redirige a la página de inicio
+
     else:
-        return HttpResponseBadRequest("Solo se aceptan solicitudes POST.")
+        # Devuelve un error si la solicitud no es de tipo POST
+        return HttpResponseBadRequest("Solo se aceptan solicitudes POST.")  # Solo acepta solicitudes POST
 
 
+
+def readHotelRender(request, hotel_id):
+    '''
+    Renderiza la vista de detalles de un hotel.
+
+    Esta función obtiene un objeto 'Hotel' por su identificador único y lo pasa al contexto para
+    renderizar la plantilla 'leerHotel.html', permitiendo mostrar la información del hotel
+    seleccionado.
+
+    :param request: Una instancia de HttpRequest que representa la solicitud entrante desde el cliente.
+    :param hotel_id: El identificador único del hotel cuyos detalles se desean visualizar.
+    :return: Una respuesta HTTP que renderiza la plantilla 'leerHotel.html' con el hotel a leer
+             en el contexto.
+    '''
+    hotel_readed = Hotel.objects.get(idHotel=hotel_id)  # Obtiene el hotel a leer
+    return render(request, "leerHotel.html",
+                  {"hotel_readed": hotel_readed})  # Renderiza la vista con el hotel a leer
+
+
+
+def readHotel(request, hotel_id):
+    '''
+    Lee y actualiza la información de un hotel existente.
+
+    Esta función obtiene un objeto 'Hotel' por su identificador único. Si el método de la solicitud
+    es POST, actualiza los detalles del hotel con los datos proporcionados en la solicitud.
+    Si la solicitud no es de tipo POST, se devuelve un error.
+
+    :param request: Una instancia de HttpRequest que representa la solicitud entrante desde el cliente.
+    :param hotel_id: El identificador único del hotel cuyo detalle se quiere leer y posiblemente actualizar.
+    :return: Una respuesta HTTP que puede ser:
+             - Redirección a la página principal después de actualizar con éxito.
+             - HTTP 400 si la solicitud no es de tipo POST.
+    '''
+    hotel = get_object_or_404(Hotel, idHotel=hotel_id)  # Obtiene el objeto Hotel por su id, o retorna 404 si no existe
+
+    if request.method == 'POST':  # Verifica si la solicitud es de tipo POST
+        # Actualiza el objeto Hotel con nuevos datos obtenidos del POST
+        hotel.nombreEstablecimiento = request.POST.get("nombreEstablecimiento")
+        hotel.horaCheckIn = request.POST.get("horaCheckIn")
+        hotel.horaCheckOut = request.POST.get("horaCheckOut")
+        hotel.petFriendly = request.POST.get("petFriendly") == 'on'  # Convierte a booleano
+        hotel.servicio = request.POST.get("servicio")
+        hotel.numeroInterior = request.POST.get("numeroInterior")
+        hotel.numeroExterior = int(request.POST.get("numeroExterior"))  # Convierte a entero
+        hotel.colonia = request.POST.get("colonia")
+        hotel.calle = request.POST.get("calle")
+        hotel.estado = request.POST.get("estado")
+
+        # Guarda los cambios en la base de datos
+        hotel.save()
+
+        # Redirige a la página principal después de editar con éxito
+        return redirect('/')  # Redirige a la página de inicio
+
+    else:
+        # Devuelve un error si la solicitud no es de tipo POST
+        return HttpResponseBadRequest("Solo se aceptan solicitudes POST.")  # Solo acepta solicitudes POST
